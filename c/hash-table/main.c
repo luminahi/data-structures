be501bddb74a15dd9c1caffdc742ccdaf204f513
Alex Lima <delimaferreiraalex@gmail.com>
2024-06-22 22:43:42 -0300

    docs: add comments

--- c/hash-table/main.c
+++ c/hash-table/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <data.h>
+#include <crypt.h>
 
 #define TABLE_SIZE 8
 
@@ -17,10 +18,53 @@ typedef struct HashTable {
 
 unsigned int hashIndex(char*);
 unsigned long hash(char*);
+
+/**
+ * Allocate memory to a new node and set key and text with the provided values.
+ * 
+ * @return A pointer to a new Node.
+ */
 Node* createNode(char*, char*);
+
+/**
+ * Insert a new Node in the hash table in a calculated index, each Node is added
+ * in the start of the linked list.
+ */
 void insertNode(HashTable*, char*, char*);
+
+/**
+ * Iterate through a buffer and get lines of 32 bytes and store in the provided 
+ * hash table, the first 3 bytes are used as a key for each inserted line.
+ * 
+ * @param hashTable the hash table to populate.
+ * @param buffer Each line of the buffer should have at most 32 bytes.
+ */
+void processData(HashTable*, char*);
+
 void printList(Node*);
+
+/**
+ * Iterate through all elements in each linked list in the hash table and 
+ * print in stdout.
+ * 
+ * @param hashTable The hash table to be printed.
+ */
 void printTable(HashTable*);
+
+/**
+ * Initialize a new hash table and set all pointers in the list to NULL,
+ * each element of the list use a simply linked list.
+ * 
+ * @return A pointer to a new hash table.
+ */
+HashTable* createTable();
+
+/**
+ * Free all resources from the provided hash table and set all 
+ * pointers in the list to NULL.
+ * 
+ * @param hashTable A hash table created with createTable.
+ */
 void destroyTable(HashTable**);
 
 Node* createNode(char* key, char* text) {
@@ -125,8 +169,8 @@ int main(int argc, char* argv[]) {
     free(buffer);
 
     printTable(hashTable);
+
     destroyTable(&hashTable);
-    printTable(hashTable);
     
     return 0;
 }
