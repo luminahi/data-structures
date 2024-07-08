1af306ce4f06d565fff25fc76002f89fbc5967b2
Alex Lima <delimaferreiraalex@gmail.com>
2024-07-08 14:39:10 -0300

    ref: move linked list to a module

--- typescript/linked-list/main.ts
+++ typescript/linked-list/main.ts
@@ -1,43 +1,4 @@
-type NodeObject<T> = {
-    value: T;
-    next: NodeObject<T> | null;
-};
-
-class LinkedList<T> {
-    private head: NodeObject<T> | null;
-
-    public constructor() {
-        this.head = null;
-    }
-
-    private createNode(value: T): NodeObject<T> {
-        return { value, next: null };
-    }
-
-    public insert(value: T): void {
-        const node = this.createNode(value);
-        node.next = this.head;
-        this.head = node;
-    }
-
-    public remove(): void {
-        if (!this.head) return;
-        this.head = this.head.next;
-    }
-
-    public print(): void {
-        let iterator = this.head;
-        while (iterator) {
-            process.stdout.write(`[${iterator.value}]-> `);
-            iterator = iterator.next;
-        }
-        process.stdout.write("\n");
-    }
-
-    public toString() {
-        return `HEAD [${this.head?.value}]`;
-    }
-}
+import { LinkedList } from "./linked-list.js";
 
 const numberList = new LinkedList<number>();
 const nameList = new LinkedList<string>();
